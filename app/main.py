8b8b3ab039ec3319c66ab5cbe51b1b4fc7183e3e
Arshil <arshilmasood@gmail.com>
2026-07-30 00:00:25 +0530

    Quality Changes

--- app/main.py
+++ app/main.py
@@ -1,14 +1,11 @@
 import time
-from fastapi import FastAPI, Request, UploadFile, File, Form
+from fastapi import FastAPI, Request
 from app.api.routes import router
 from app.api.dependencies import create_db_and_tables
 from contextlib import asynccontextmanager
 from app.db.database import engine
 from fastapi.middleware.cors import CORSMiddleware
 from app.core import setup_logging, get_logger
-
-#--app initialization--
-app = FastAPI
 
 # Setup logging before app creation
 setup_logging()
@@ -58,8 +55,3 @@ app.include_router(router, prefix="/api", tags=["API"])
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
-
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host = "0.0.0.0", port=8000)
